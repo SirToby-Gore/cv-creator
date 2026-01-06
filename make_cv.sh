3f54ffdbbe0bd5e054202ef526901129f7e170f7
Toby Gore <tobyedwardgore+git@gmail.com>
2026-01-06 10:31:39 +0000

    imporved cli

--- make_cv.sh
+++ make_cv.sh
@@ -1,11 +1,32 @@
 #!/usr/bin/env bash
 
-php src/php/index.php > index.html
+DIR="$(dirname "$0")"
 
-if [[ $# < 1 ]]; then
-       	echo "too few args provided, requires 1 arg page size to convert a html to pdf"
-	exit 1       
+if [[ $# > 2 ]]; then
+	echo
+	echo "too many args provided, requires 2 args, page size and json path"
+	exit 1
 fi
 
-wkhtmltopdf --page-size "$1" index.html cv.pdf
+if [[ $# < 2 ]]; then
+	echo
+	echo "too few args provided, requires 2 args page size and json path"
+	exit 1
+fi
 
+if [[ ! -f "$2" ]]; then
+	echo
+	echo "file path \"$2\" does not exist"
+	exit 1
+fi
+
+INPUT="$(pwd)/$2"
+
+echo
+echo "INFO: creating CV from \"$INPUT\", using page size $1"
+echo
+
+rm "$DIR/cv.json"
+ln -s "$INPUT" "$DIR/cv.json"
+php "$DIR/src/php/index.php" > "$DIR/index.html"
+wkhtmltopdf --page-size "$1" "$DIR/index.html" "./cv.pdf"
